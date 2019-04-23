382c9d1d08bd930f1d20b25f2ce8a4636fae8c35
Andrew Waller <andrewhwaller@gmail.com>
2019-04-23 00:42:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    get '/new' do
-      erb :new
+    get '/' do
+      
+    end
+
+    get '/pirates' do
+      erb :show
       end
     end
 end
